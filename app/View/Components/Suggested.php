41527c77b72b77e1a821f826b144982f306976cf
Marcis-Zvirgzdins <mz24032@edu.lu.lv>
2025-06-08 16:29:45 +0300

    Pievienota komentāru sadaļa katram rakstam

---
+++ app/View/Components/Suggested.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Suggested extends Component
+{
+    public $articles;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($articles)
+    {
+        $this->articles = $articles;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.suggested');
+    }
+}
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -16,4 +16,35 @@
     <div>
         <p>{{ $article->content }}</p>
     </div>
+
+
+    <!-- Komentāru sadaļa -->
+    @auth
+        <form action="{{ route('comments.store', $article->id) }}" method="POST">
+            @csrf
+            <div>
+                <label for="content">Pievienot komentāru:</label>
+                <textarea id="content" name="content" rows="3" required></textarea>
+            </div>
+            <button type="submit">Pievienot</button>
+        </form>
+    @else
+        <p><a href="{{ route('login') }}">Pieslēdzaties</a>, lai pievienotu komentāru</p>
+    @endauth
+
+
+    <h2>Komentāri</h2>
+    @foreach($article->comments as $comment)
+        <div class="comment">
+            @if($comment->user->profile_picture)
+                <img src="{{ asset('storage/' . $comment->user->profile_picture) }}" alt="Profile Picture">
+            @else
+                <img src="{{ asset('assets/user-icon-blank-512.png') }}" alt="Profile Picture">
+            @endif
+            <p>{{ $comment->user->username }}</p>
+            <p>{{ $comment->content }}</p>
+
+            <p>Pievienots: {{ $comment->created_at->format('M d, Y \a\t H:i') }}</p>
+        </div>
+    @endforeach
 </x-layout>
