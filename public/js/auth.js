61f335723d16d56c45967b2e29a7237235fc9014
Marcis-Zvirgzdins <mz24032@edu.lu.lv>
2025-06-15 17:39:33 +0300

    izlabota kļūda

--- public/js/auth.js
+++ public/js/auth.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const img = this.querySelector('img');
             img.src = isPassword 
                 ? "/public/icons/visible-off-w-32.svg"
-                : "/public/icons/icons/visible-w-32.svg";
+                : "/public/icons/visible-w-32.svg";
         });
     });
 });
