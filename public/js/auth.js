f9e1abff9dd49572030dbebe75357ef78c204c2c
Marcis-Zvirgzdins <mz24032@edu.lu.lv>
2025-06-15 17:55:29 +0300

    Vēlreiz izlabots auth.js (cerams)

--- public/js/auth.js
+++ public/js/auth.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const img = this.querySelector('img');
             img.src = isPassword 
-                ? "/public/icons/visible-off-w-32.svg"
-                : "/public/icons/visible-w-32.svg";
+                ? "/icons/visible-off-w-32.svg"
+                : "/icons/visible-w-32.svg";
         });
     });
 });
